9addb71d9f088fdc760602c192f16937fe9f654a
gideon-del <gideonchidi471@gmail.com>
2023-11-23 20:16:02 +0100

    chore: invoice page design done
    Remains the void and suspend modal

--- client/app/(dashboard)/client/[id]/InvoiceRow.jsx
+++ client/app/(dashboard)/client/[id]/InvoiceRow.jsx
@@ -2,9 +2,147 @@
 import { useRef, useState } from "react";
 import { poppins } from "@/utils/fonts";
 import Link from "next/link";
-
+import { Dialog } from "@headlessui/react";
+const EditModal = ({ isOpen, closeModal }) => {
+  return (
+    <Dialog open={isOpen} onClose={closeModal}>
+      <div
+        className="fixed z-50 inset-0 bg-black/30 w-screen h-screen grid place-items-center"
+        aria-hidden="true"
+      >
+        <Dialog.Panel className="bg-white text-dark p-6 rounded-lg w-[90%] max-w-2xl ">
+          <Dialog.Title className={"flex items-center justify-between mb-5"}>
+            <h1 className="text-base font-bold tracking-[0.32px]">
+              Edit Invoice
+            </h1>
+            <button className="text-grey" onClick={closeModal}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M18 6L6 18M6 6L18 18"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </Dialog.Title>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="grid grid-cols-2 gap-8  mb-8">
+              <div className="flex flex-col gap-2  ">
+                <label
+                  htmlFor="name"
+                  className="text-sm text-dark  font-medium leading-6"
+                >
+                  Client Name
+                </label>
+                <select
+                  placeholder="Toyosi Lawal"
+                  className="px-4 py-3 rounded-lg border border-[#D0D5DD] shadow-sm placeholder:text-grey text-dark text-base leading-6 font-normal"
+                  id="name"
+                  name="name"
+                >
+                  <option
+                    value=""
+                    className="text-grey"
+                    hidden
+                    disabled
+                    selected
+                  >
+                    Select Client Name
+                  </option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2  ">
+                <label
+                  htmlFor="dueDate"
+                  className="text-sm text-dark  font-medium leading-6"
+                >
+                  Due Date*
+                </label>
+                <input
+                  placeholder="toyosi@gmail.com"
+                  type="date"
+                  className="px-4 py-3 rounded-lg border border-[#D0D5DD] shadow-sm placeholder:text-grey text-dark text-base leading-6 font-normal"
+                  id="dueDate"
+                  name="dueDate"
+                />
+              </div>
+              <div className="flex flex-col gap-2  ">
+                <label
+                  htmlFor="amount"
+                  className="text-sm text-dark  font-medium leading-6"
+                >
+                  Amount*
+                </label>
+                <input
+                  placeholder="#200,000.00"
+                  type="number"
+                  name="amount"
+                  id="amount"
+                  className="px-4 py-3 rounded-lg border border-[#D0D5DD] shadow-sm placeholder:text-grey text-dark text-base leading-6 font-normal"
+                />
+              </div>
+              <div className="flex flex-col gap-2  ">
+                <label
+                  htmlFor="paymentFor"
+                  className="text-sm text-dark  font-medium leading-6"
+                >
+                  Payment For
+                </label>
+                <input
+                  placeholder="Wedding Coverage"
+                  id="paymentFor"
+                  name="paymentFor"
+                  className="px-4 py-3 rounded-lg border border-[#D0D5DD] shadow-sm placeholder:text-grey text-dark text-base leading-6 font-normal"
+                />
+              </div>
+              <div className="flex flex-col gap-2 col-span-2   ">
+                <label
+                  htmlFor="description"
+                  className="text-sm text-dark  font-medium leading-6  "
+                >
+                  Description*
+                </label>
+                <textarea
+                  placeholder="This is the payment for the complete prewedding & video coverage of your wedding."
+                  id="description"
+                  name="description"
+                  className="px-4 py-3 rounded-lg w-full border border-[#D0D5DD] shadow-sm placeholder:text-grey text-dark text-base leading-6 font-normal min-h-[100px]"
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-3">
+              <button
+                type="submit"
+                className="bg-primary py-3 px-5 rounded-lg border border-[#D0D5DD] text-secondary flex justify-center items-center text-base font-medium leading-6"
+              >
+                {" "}
+                Update Invoice
+              </button>
+              <button
+                type="submit"
+                className="bg-error py-3 px-5 rounded-lg border border-[#7F56D9] text-white flex justify-center items-center text-base font-medium leading-6"
+                onClick={closeModal}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </Dialog.Panel>
+      </div>
+    </Dialog>
+  );
+};
 const InvoiceRow = ({ color }) => {
   const [showActions, setShowActions] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const actionRef = useRef(null);
   const toggleActions = () => {
     setShowActions((prev) => {
@@ -14,6 +152,13 @@ const InvoiceRow = ({ color }) => {
       return !prev;
     });
   };
+  const openEditModal = () => {
+    setShowActions(false);
+    setShowEditModal(true);
+  };
+  const closeEditModal = () => {
+    setShowEditModal(false);
+  };
   return (
     <tr
       className={`border-collapse border border-grey ${
@@ -22,9 +167,7 @@ const InvoiceRow = ({ color }) => {
     >
       <td>
         <div className="   items-center gap-2 text-dark font-normal text-sm  tracking-[0.28px] mr-10 py-4 pl-4 text-center">
-          <Link href={"/client"} className="underline">
-            IN01
-          </Link>
+          <span>IN01</span>
         </div>
       </td>
       <td>
@@ -49,8 +192,8 @@ const InvoiceRow = ({ color }) => {
       </td>
       <td>
         <div className="  items-center gap-2 text-dark font-normal text-sm  tracking-[0.28px]  py-4 text-center ">
-          <span className="bolck  px-5 rounded-3xl bg-primary text-secondary flex items-center justify-center ">
-            {" "}
+          <span className="  py-2 w-fit px-4 mx-4 rounded-3xl bg-primary text-secondary ">
+            Pending
           </span>
         </div>
       </td>
@@ -105,7 +248,7 @@ const InvoiceRow = ({ color }) => {
         <div
           className={`absolute px-4  right-4 rounded-lg transition-all duration-500 ease-in-out overflow-hidden  bg-white min-w-[100px] w-fit shadow-lg ${
             showActions ? "py-2 h-fit" : "py-0 h-0 "
-          } z-50`}
+          } z-50 whitespace-nowrap`}
           ref={actionRef}
         >
           <div
@@ -114,7 +257,10 @@ const InvoiceRow = ({ color }) => {
             <button className="text-[#292D32] text-start">
               Send Reminder{" "}
             </button>
-            <button className=" text-start text-[#292D32]">
+            <button
+              className=" text-start text-[#292D32]"
+              onClick={openEditModal}
+            >
               Extend due date
             </button>
             <button className=" text-start text-[#292D32]">Void Invoice</button>
@@ -122,6 +268,7 @@ const InvoiceRow = ({ color }) => {
               Suspend Invoice
             </button>
           </div>
+          <EditModal isOpen={showEditModal} closeModal={closeEditModal} />
         </div>
       </td>
     </tr>
