9c676e74269d1a0bb034e80bfa21724042f22112
gideon-del <gideonchidi471@gmail.com>
2023-11-26 05:24:55 +0100

    feat: added user context

--- client/context/User.jsx
+++ client/context/User.jsx
@@ -1,1 +1,36 @@
 "use client";
+
+import { createContext, useEffect, useState } from "react";
+
+const UserCtx = createContext({
+  user: {},
+  isLoggedIn: false,
+  isLoading: true,
+  login: () => {},
+  logout: () => {},
+});
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const login = (user) => {
+    setUser(user);
+    // localStorage.setItem('invc_tk')
+  };
+  logout = () => {
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("invc_tk"));
+    if (token) {
+    }
+  }, []);
+  return (
+    <UserCtx.Provider
+      value={{ user, login, logout, isLoading: loading, isLoggedIn: !!user }}
+    >
+      {children}
+    </UserCtx.Provider>
+  );
+};
